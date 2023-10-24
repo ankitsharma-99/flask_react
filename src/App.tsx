39e8334a35ff22074846badcf9d45cdcc47ba1cf
Ankit Sharma <ankitsharma81242@gmail.com>
2023-10-25 00:18:45 +0530

    initial client code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/members")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.members);
+        console.log(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {typeof data === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        data.map((member, i) => {
+          return <p key={i}>{member}</p>;
+        })
+      )}
     </div>
   );
-}
+};
 
 export default App;
